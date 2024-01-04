84b1e4b4184532464e70fd2795dd5b6c9524bd84
熊川瑛至 <kumakawa@KumacBook.local>
2024-01-05 00:02:00 +0900

    Add AnswerPage

--- src/app/AnswerPage/page.tsx
+++ src/app/AnswerPage/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import TextArea from '../components/TextArea'
 
-export default function SinglePlay() {
+export default function AnswerPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Image
@@ -13,11 +13,10 @@ export default function SinglePlay() {
         height={300}
         priority
       />
-      <TextArea 
-        placeholder="Enter your prompt"
-      />
-      <Link href="/AnswerPage">
-        予測する
+      <h1>hogehogehoge
+      </h1>
+      <Link href="/">
+        戻る
       </Link>
     </main>
   )
