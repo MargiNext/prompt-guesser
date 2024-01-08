5ce48bb31dceab66f5b64cb6badacc45649b3054
ksawaray <sawara04.o@gmail.com>
2024-01-08 22:53:13 +0900

    rename api name: problem -> question

--- src/pages/api/question.ts
+++ src/pages/api/question.ts
@@ -1,9 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-// return problem(prompt, img)
+// return question(id, prompt, img)
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  // parse request
   const { query, method } = req
+  // parse request
   const id = parseInt(query.id as string, 10)
 
   res.status(200).json({ id: id, prompt: 'hogehoge', img: 'public/image.png'})
