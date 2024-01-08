ba072d206c3292081a9b23ca585f2ffbd6f536e2
morinokumakamesan <morinokumakamesan@gmail.com>
2024-01-08 17:36:50 +0900

    Debug Redux v11

--- src/app/provide.tsx
+++ src/app/provide.tsx
@@ -3,7 +3,7 @@
 import { Provider } from 'react-redux'
 import { store } from "./store";
 
-export default ({children}) => {
+export default function provide({children}) {
   return (
     <Provider store={store}>
       {children}
