2319160b9e043c2e956e10b0cc0ed7819b079666
morinokumakamesan <morinokumakamesan@gmail.com>
2024-01-08 16:48:47 +0900

    Debug Redux v7

--- src/app/AnswerPage/page.tsx
+++ src/app/AnswerPage/page.tsx
@@ -6,15 +6,18 @@ import TextArea from '../components/TextArea'
 import fetch from 'node-fetch'
 import { useSelector } from 'react-redux';
 import { RootState } from '../store'
+import { Provider } from 'react-redux'
+import { store } from '../store'
 
 interface AnswerState {
   answer: string;
 }
 
 export default function AnswerPage() {
-  const textData = useSelector((state: RootState) => state.answer.text)
-  console.log('AnswerPage:' + textData)
+  // const textData = useSelector((state: RootState) => state.answer.text)
+  // console.log('AnswerPage:' + textData)
   return (
+    <Provider store={store}>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Image
         src="/image.png"
@@ -23,10 +26,10 @@ export default function AnswerPage() {
         height={300}
         priority
       />
-       <p>あなたのプロンプト: {textData}</p>
+       {/* <p>あなたのプロンプト: {textData}</p> */}
       <Link href="/">
         戻る
       </Link>
-    </main>
+    </main></Provider>
   )
 }
