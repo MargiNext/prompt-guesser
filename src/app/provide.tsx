0d1cbf85f55a4a1d41c9e6e519af091bdca608a4
morinokumakamesan <morinokumakamesan@gmail.com>
2024-01-08 17:44:27 +0900

    Debug Redux v12

--- src/app/provide.tsx
+++ src/app/provide.tsx
@@ -1,9 +1,10 @@
 "use client";
 // import { Provider } from "react-redux";
+import React from 'react'
 import { Provider } from 'react-redux'
 import { store } from "./store";
 
-export default function provide({children}) {
+export default function provide({children}: {children: React.ReactNode}) {
   return (
     <Provider store={store}>
       {children}
