0f97fb8fd2bb171fb59c84e642df10d0dd5bca17
ksawaray <sawara04.o@gmail.com>
2024-01-11 22:17:43 +0900

    dynamodbからデータを取得して、その値を返すようにした

--- src/pages/api/question.ts
+++ src/pages/api/question.ts
@@ -1,10 +1,46 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { fromEnv } from '@aws-sdk/credential-providers'
+import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb'
+import { unmarshall } from '@aws-sdk/util-dynamodb'
 
 // return question(id, prompt, img)
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { query, method } = req
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // parse request
-  const id = parseInt(query.id as string, 10)
+  const { query, _method } = req
 
-  res.status(200).json({ id: id, prompt: 'hogehoge', img: 'public/image.png'})
+  // sanitizing
+  let question_id
+  if(isNaN(parseInt(query.id as string, 10))) {
+    return res.status(400).json({ error: "ID is invalid." })
+  } else {
+    question_id = parseInt(query.id as string, 10)
+  }
+
+  const client = new DynamoDBClient({
+    credentials: fromEnv(),
+    region: 'ap-northeast-1',
+  })
+  const table_name = "questions"
+
+  const input = {
+    "Key": {
+      "id": {
+        "N": `${question_id}`
+      }
+    },
+    "TableName": `${table_name}`
+  }
+
+  const command = new GetItemCommand(input);
+
+  let item
+  // get question data
+  try {
+    const data = await client.send(command)
+    item = unmarshall(data.Item)
+  } catch (error) {
+    return res.status(400).json({ error: "Failed to get question data from dynamodb." })
+  }
+
+  return res.status(200).json({ id: question_id, prompt: item.prompt, img: item.img})
 }
