63c86f0dd786167d510c01827acaf479a1559dac
ksawaray <sawara04.o@gmail.com>
2024-01-14 14:50:54 +0900

    question apiのリファクタリング

--- src/pages/api/question.ts
+++ src/pages/api/question.ts
@@ -3,32 +3,38 @@ import { fromEnv } from '@aws-sdk/credential-providers'
 import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb'
 import { unmarshall } from '@aws-sdk/util-dynamodb'
 
-// return question(id, prompt, img)
+/*
+  * return question
+  * -------------------
+  * input   : id
+  * output  : prompt
+  *         : img
+*/
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // parse request
   const { query, _method } = req
 
   // sanitizing
-  let question_id
+  let questionId
   if(isNaN(parseInt(query.id as string, 10))) {
     return res.status(400).json({ error: "ID is invalid." })
   } else {
-    question_id = parseInt(query.id as string, 10)
+    questionId = parseInt(query.id as string, 10)
   }
 
   const client = new DynamoDBClient({
     credentials: fromEnv(),
     region: 'ap-northeast-1',
   })
-  const table_name = "questions"
+  const tableName = "questions"
 
   const input = {
     "Key": {
       "id": {
-        "N": `${question_id}`
+        "N": `${questionId}`
       }
     },
-    "TableName": `${table_name}`
+    "TableName": `${tableName}`
   }
 
   const command = new GetItemCommand(input);
@@ -42,5 +48,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(400).json({ error: "Failed to get question data from dynamodb." })
   }
 
-  return res.status(200).json({ id: question_id, prompt: item.prompt, img: item.img})
+  return res.status(200).json({ id: questionId, prompt: item.prompt, img: item.img})
 }
