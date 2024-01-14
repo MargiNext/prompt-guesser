5d27e4cb6ca974393cf69ddc7dc631ec38ed19af
ksawaray <sawara04.o@gmail.com>
2024-01-14 16:44:40 +0900

    question apiでbase64変換した画像を返すように変更

--- src/pages/api/question.ts
+++ src/pages/api/question.ts
@@ -2,6 +2,60 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { fromEnv } from '@aws-sdk/credential-providers'
 import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb'
 import { unmarshall } from '@aws-sdk/util-dynamodb'
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3'
+
+async function getQuestionInfo(questionId: int): Promise<any> {
+  const client = new DynamoDBClient({
+    credentials: fromEnv(),
+    region: 'ap-northeast-1',
+  })
+  const tableName = "questions"
+
+  const input = {
+    "Key": {
+      "id": {
+        "N": `${questionId}`
+      }
+    },
+    "TableName": `${tableName}`
+  }
+
+  const command = new GetItemCommand(input)
+
+  let item
+  try {
+    const response = await client.send(command)
+    item = unmarshall(response.Item)
+  } catch (error) {
+    res.status(400).json({ error: "Failed to get question item from dynamodb." })
+  }
+
+  return item
+}
+
+async function getImage(key: string): Promise<any> {
+  const client = new S3Client({
+    credentials: fromEnv(),
+    region: 'ap-northeast-1',
+  })
+
+  const input = {
+    "Bucket": "prompt-guesser",
+    "Key": `${key}`
+  }
+
+  const command = new GetObjectCommand(input)
+
+  let imageData
+  try {
+    const response = await client.send(command)
+    imageData = await response.Body.transformToString("base64")
+  } catch (error) {
+    res.status(400).json({ error: "Failed to get image from s3." })
+  }
+
+  return imageData
+}
 
 /*
   * return question
@@ -22,31 +76,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     questionId = parseInt(query.id as string, 10)
   }
 
-  const client = new DynamoDBClient({
-    credentials: fromEnv(),
-    region: 'ap-northeast-1',
-  })
-  const tableName = "questions"
-
-  const input = {
-    "Key": {
-      "id": {
-        "N": `${questionId}`
-      }
-    },
-    "TableName": `${tableName}`
+  let prompt, img
+  const getQuestion = async() => {
+    const item = await getQuestionInfo(questionId)
+    prompt = item.prompt
+    const imgKey = item.img
+    img = await getImage(imgKey)
   }
 
-  const command = new GetItemCommand(input);
+  await getQuestion()
 
-  let item
-  // get question data
-  try {
-    const data = await client.send(command)
-    item = unmarshall(data.Item)
-  } catch (error) {
-    return res.status(400).json({ error: "Failed to get question data from dynamodb." })
-  }
-
-  return res.status(200).json({ id: questionId, prompt: item.prompt, img: item.img})
+  return res.status(200).json({ id: questionId, prompt: prompt, img: img})
 }
